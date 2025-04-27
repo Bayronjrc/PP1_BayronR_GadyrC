a0163f92c79d7a94f630dcd13d11902425977c6f
Bayron Rodriguez Centeno <bayronrodriguez4@gmail.com>
2025-04-26 22:47:52 -0600

    gracia papa dios

---
+++ Programa/bin/main/flex/Scanner.flex
@@ -0,0 +1,115 @@
+import java_cup.runtime.*;
+import parser.sym;
+
+%%
+
+/* Opciones y declaraciones */
+%class Scanner
+%unicode
+%cup
+%line
+%column
+%public
+
+%{
+  /* Código que se incluirá en el scanner */
+  private Symbol symbol(int type) {
+    return new Symbol(type, yyline+1, yycolumn+1);
+  }
+
+  private Symbol symbol(int type, Object value) {
+    return new Symbol(type, yyline+1, yycolumn+1, value);
+  }
+%}
+
+/* Macros para expresiones regulares */
+LineTerminator = \r|\n|\r\n
+WhiteSpace     = {LineTerminator} | [ \t\f]
+Identifier     = [a-zA-Z][a-zA-Z0-9_]*
+IntLiteral     = [0-9]+
+FloatLiteral   = [0-9]+\.[0-9]+
+CharLiteral    = \'[^\']\'
+StringLiteral  = \"[^\"]*\"
+CommentLine    = "//".*
+CommentBlock   = "/*" ~"*/"
+
+%%
+
+/* Reglas léxicas */
+
+/* Palabras reservadas */
+"if"        { return symbol(sym.IF); }
+"elif"      { return symbol(sym.ELIF); }
+"else"      { return symbol(sym.ELSE); }
+"do"        { return symbol(sym.DO); }
+"while"     { return symbol(sym.WHILE); }
+"for"       { return symbol(sym.FOR); }
+"break"     { return symbol(sym.BREAK); }
+"return"    { return symbol(sym.RETURN); }
+"int"       { return symbol(sym.INT); }
+"float"     { return symbol(sym.FLOAT); }
+"bool"      { return symbol(sym.BOOL); }
+"char"      { return symbol(sym.CHAR); }
+"string"    { return symbol(sym.STRING); }
+"void"      { return symbol(sym.VOID); }
+"read"      { return symbol(sym.READ); }
+"write"     { return symbol(sym.WRITE); }
+"main"      { return symbol(sym.MAIN); }
+"true"      { return symbol(sym.TRUE, Boolean.TRUE); }
+"false"     { return symbol(sym.FALSE, Boolean.FALSE); }
+
+/* Operadores aritméticos */
+"+"         { return symbol(sym.PLUS); }
+"-"         { return symbol(sym.MINUS); }
+"*"         { return symbol(sym.TIMES); }
+"/"         { return symbol(sym.DIVIDE); }
+"%"         { return symbol(sym.MOD); }
+"^"         { return symbol(sym.POW); }
+"++"        { return symbol(sym.INCREMENT); }
+"--"        { return symbol(sym.DECREMENT); }
+
+/* Operadores relacionales */
+"<"         { return symbol(sym.LT); }
+"<="        { return symbol(sym.LTE); }
+">"         { return symbol(sym.GT); }
+">="        { return symbol(sym.GTE); }
+"=="        { return symbol(sym.EQ); }
+"!="        { return symbol(sym.NEQ); }
+
+/* Operadores lógicos */
+"&&"        { return symbol(sym.AND); }
+"||"        { return symbol(sym.OR); }
+"!"         { return symbol(sym.NOT); }
+
+/* Delimitadores */
+"("         { return symbol(sym.LPAREN); }
+")"         { return symbol(sym.RPAREN); }
+"{"         { return symbol(sym.LBLOCK); }
+"}"         { return symbol(sym.RBLOCK); }
+"["         { return symbol(sym.LBRACKET); }
+"]"         { return symbol(sym.RBRACKET); }
+"?"         { return symbol(sym.QUESTION); }
+","         { return symbol(sym.COMMA); }
+"="         { return symbol(sym.ASSIGN); }
+
+/* Comentarios */
+{CommentLine}    { return symbol(sym.COMMENT_LINE); }
+"/*"            { return symbol(sym.LCOMMENT_BLOCK); }
+"*/"            { return symbol(sym.RCOMMENT_BLOCK); }
+
+/* Valores literales */
+{IntLiteral}    { return symbol(sym.LIT_INT, Integer.parseInt(yytext())); }
+{FloatLiteral}  { return symbol(sym.LIT_FLOAT, Float.parseFloat(yytext())); }
+{CharLiteral}   { return symbol(sym.LIT_CHAR, yytext().charAt(1)); }
+{StringLiteral} { return symbol(sym.LIT_STRING, yytext().substring(1, yytext().length() - 1)); }
+
+/* Identificadores */
+{Identifier}    { return symbol(sym.ID, yytext()); }
+
+/* Espacios en blanco */
+{WhiteSpace}    { /* ignorar */ }
+
+/* Error para cualquier otro carácter */
+[^]             { 
+                  System.out.println("Error léxico: Carácter ilegal <" + yytext() + "> en línea " + (yyline+1) + ", columna " + (yycolumn+1)); 
+                }
--- Programa/src/main/java/main/Main.java
+++ Programa/src/main/java/main/Main.java
@@ -1,9 +1,8 @@
-package main;
+package main.java.main;
 
 import java.io.*;
-import lexer.Scanner;
-import parser.parser;
-import parser.sym;
+import main.java.lexer.Scanner;
+import main.java.parser.parser;
 import java_cup.runtime.Symbol;
 
 public class Main {
@@ -62,64 +61,64 @@ public class Main {
     }
     
     // Método auxiliar para convertir número de símbolo a su nombre
-    private static String symbolToString(int sym) {
+    public static String symbolToString(int sym) {
         // Mapeo de símbolos a nombres según parser.cup
         switch(sym) {
-            case parser.sym.ID: return "ID";
-            case parser.sym.LIT_INT: return "INT_LITERAL";
-            case parser.sym.LIT_FLOAT: return "FLOAT_LITERAL";
-            case parser.sym.LIT_CHAR: return "CHAR_LITERAL";
-            case parser.sym.LIT_STRING: return "STRING_LITERAL";
-            case parser.sym.LIT_BOOL: return "BOOL_LITERAL";
-            case parser.sym.TRUE: return "TRUE";
-            case parser.sym.FALSE: return "FALSE";
-            case parser.sym.IF: return "IF";
-            case parser.sym.ELIF: return "ELIF";
-            case parser.sym.ELSE: return "ELSE";
-            case parser.sym.DO: return "DO";
-            case parser.sym.WHILE: return "WHILE";
-            case parser.sym.FOR: return "FOR";
-            case parser.sym.BREAK: return "BREAK";
-            case parser.sym.RETURN: return "RETURN";
-            case parser.sym.INT: return "INT";
-            case parser.sym.FLOAT: return "FLOAT";
-            case parser.sym.BOOL: return "BOOL";
-            case parser.sym.CHAR: return "CHAR";
-            case parser.sym.STRING: return "STRING";
-            case parser.sym.VOID: return "VOID";
-            case parser.sym.READ: return "READ";
-            case parser.sym.WRITE: return "WRITE";
-            case parser.sym.MAIN: return "MAIN";
-            case parser.sym.PLUS: return "PLUS";
-            case parser.sym.MINUS: return "MINUS";
-            case parser.sym.TIMES: return "TIMES";
-            case parser.sym.DIVIDE: return "DIVIDE";
-            case parser.sym.MOD: return "MOD";
-            case parser.sym.POW: return "POW";
-            case parser.sym.INCREMENT: return "INCREMENT";
-            case parser.sym.DECREMENT: return "DECREMENT";
-            case parser.sym.LT: return "LT";
-            case parser.sym.LTE: return "LTE";
-            case parser.sym.GT: return "GT";
-            case parser.sym.GTE: return "GTE";
-            case parser.sym.EQ: return "EQ";
-            case parser.sym.NEQ: return "NEQ";
-            case parser.sym.AND: return "AND";
-            case parser.sym.OR: return "OR";
-            case parser.sym.NOT: return "NOT";
-            case parser.sym.LPAREN: return "LPAREN";
-            case parser.sym.RPAREN: return "RPAREN";
-            case parser.sym.LBLOCK: return "LBLOCK";
-            case parser.sym.RBLOCK: return "RBLOCK";
-            case parser.sym.QUESTION: return "QUESTION";
-            case parser.sym.COMMA: return "COMMA";
-            case parser.sym.ASSIGN: return "ASSIGN";
-            case parser.sym.COMMENT_LINE: return "COMMENT_LINE";
-            case parser.sym.LCOMMENT_BLOCK: return "LCOMMENT_BLOCK";
-            case parser.sym.RCOMMENT_BLOCK: return "RCOMMENT_BLOCK";
-            case parser.sym.LBRACKET: return "LBRACKET";
-            case parser.sym.RBRACKET: return "RBRACKET";
-            case parser.sym.UMINUS: return "UMINUS";
+            case main.java.parser.sym.ID: return "ID";
+            case main.java.parser.sym.LIT_INT: return "INT_LITERAL";
+            case main.java.parser.sym.LIT_FLOAT: return "FLOAT_LITERAL";
+            case main.java.parser.sym.LIT_CHAR: return "CHAR_LITERAL";
+            case main.java.parser.sym.LIT_STRING: return "STRING_LITERAL";
+            case main.java.parser.sym.LIT_BOOL: return "BOOL_LITERAL";
+            case main.java.parser.sym.TRUE: return "TRUE";
+            case main.java.parser.sym.FALSE: return "FALSE";
+            case main.java.parser.sym.IF: return "IF";
+            case main.java.parser.sym.ELIF: return "ELIF";
+            case main.java.parser.sym.ELSE: return "ELSE";
+            case main.java.parser.sym.DO: return "DO";
+            case main.java.parser.sym.WHILE: return "WHILE";
+            case main.java.parser.sym.FOR: return "FOR";
+            case main.java.parser.sym.BREAK: return "BREAK";
+            case main.java.parser.sym.RETURN: return "RETURN";
+            case main.java.parser.sym.INT: return "INT";
+            case main.java.parser.sym.FLOAT: return "FLOAT";
+            case main.java.parser.sym.BOOL: return "BOOL";
+            case main.java.parser.sym.CHAR: return "CHAR";
+            case main.java.parser.sym.STRING: return "STRING";
+            case main.java.parser.sym.VOID: return "VOID";
+            case main.java.parser.sym.READ: return "READ";
+            case main.java.parser.sym.WRITE: return "WRITE";
+            case main.java.parser.sym.MAIN: return "MAIN";
+            case main.java.parser.sym.PLUS: return "PLUS";
+            case main.java.parser.sym.MINUS: return "MINUS";
+            case main.java.parser.sym.TIMES: return "TIMES";
+            case main.java.parser.sym.DIVIDE: return "DIVIDE";
+            case main.java.parser.sym.MOD: return "MOD";
+            case main.java.parser.sym.POW: return "POW";
+            case main.java.parser.sym.INCREMENT: return "INCREMENT";
+            case main.java.parser.sym.DECREMENT: return "DECREMENT";
+            case main.java.parser.sym.LT: return "LT";
+            case main.java.parser.sym.LTE: return "LTE";
+            case main.java.parser.sym.GT: return "GT";
+            case main.java.parser.sym.GTE: return "GTE";
+            case main.java.parser.sym.EQ: return "EQ";
+            case main.java.parser.sym.NEQ: return "NEQ";
+            case main.java.parser.sym.AND: return "AND";
+            case main.java.parser.sym.OR: return "OR";
+            case main.java.parser.sym.NOT: return "NOT";
+            case main.java.parser.sym.LPAREN: return "LPAREN";
+            case main.java.parser.sym.RPAREN: return "RPAREN";
+            case main.java.parser.sym.LBLOCK: return "LBLOCK";
+            case main.java.parser.sym.RBLOCK: return "RBLOCK";
+            case main.java.parser.sym.QUESTION: return "QUESTION";
+            case main.java.parser.sym.COMMA: return "COMMA";
+            case main.java.parser.sym.ASSIGN: return "ASSIGN";
+            case main.java.parser.sym.COMMENT_LINE: return "COMMENT_LINE";
+            case main.java.parser.sym.LCOMMENT_BLOCK: return "LCOMMENT_BLOCK";
+            case main.java.parser.sym.RCOMMENT_BLOCK: return "RCOMMENT_BLOCK";
+            case main.java.parser.sym.LBRACKET: return "LBRACKET";
+            case main.java.parser.sym.RBRACKET: return "RBRACKET";
+            case main.java.parser.sym.UMINUS: return "UMINUS";
             default: return "SYMBOL_" + sym;
         }
     }
