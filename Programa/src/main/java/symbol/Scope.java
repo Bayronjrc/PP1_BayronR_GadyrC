71815589b7c8ffd6153cd9031dd62276a4170a2b
Bayron Rodriguez Centeno <bayronrodriguez4@gmail.com>
2025-06-01 12:00:56 -0600

    se agrega una nueva clase de alcance

---
+++ Programa/src/main/java/symbol/Scope.java
@@ -0,0 +1,17 @@
+package main.java.symbol;
+
+import java.util.*;
+
+/**
+ * Clase que representa un alcance (scope) específico en el programa.
+ * 
+ * Cada alcance mantiene su propia tabla de simbolos y una referencia
+ * al alcance padre, permitiendo la búsqueda jerárquica de simbolos
+ * siguiendo las reglas de visibilidad del lenguaje.
+ * 
+ * @author Compilador
+ * @version 1.0
+ */
+public class Scope {
+    
+}
