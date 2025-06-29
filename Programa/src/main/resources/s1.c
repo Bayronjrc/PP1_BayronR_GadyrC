493efc04421510416e73a1fdc2b97d8c8f69c819
Bayron Rodriguez Centeno <bayronrodriguez4@gmail.com>
2025-06-29 08:44:28 -0600

    ver estable 2.0

--- Programa/src/main/resources/s1.c
+++ Programa/src/main/resources/s1.c
@@ -1,9 +1,34 @@
-int sumarDos ʃ int x ʅ \
-    int resultado | x + 2?
-    return resultado?
+int suma ʃ int a, int b ʅ \
+    int resultado | a + b ?
+    return resultado ?
+/
+
+int factorial ʃ int n ʅ \
+    if ʃ n <= 1 ʅ \
+        return 1 ?
+    /
+    int temp | n - 1 ?
+    int recurse | factorial ʃ temp ʅ ?
+    int resultado | n * recurse ?
+    return resultado ?
+/
+
+void testBasico ʃ ʅ \
+    int x | 5 ?
+    int y | 3 ?
+    
+    int resultado1 | suma ʃ x, y ʅ ?
+    write ʃ resultado1 ʅ ? 
+    
+    int resultado2 | factorial ʃ 4 ʅ ?
+    write ʃ resultado2 ʅ ? 
+    
+
+    for ʃ int i | 0 ? i < 3 ? ++i ʅ \
+        write ʃ i ʅ ?
+    /
 /
 
 void main ʃ ʅ \
-    int num | sumarDos ʃ 10 ʅ?
-    write ʃ num ʅ?
+    testBasico ʃ ʅ ?
 /
